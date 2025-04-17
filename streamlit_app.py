e4ffc600ad774f561476afa6319dec7fb6745301
Kelompok-2-PLI <wrixteryrlme@gmail.com>
2025-04-17 10:51:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 norma tendangan madun")
+st.title("🎈 madun bau limbah")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
